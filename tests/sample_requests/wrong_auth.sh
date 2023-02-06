a283b33c75e0286e291afde491488136231261b2
jojopko <gamerat.github@gmail.com>
2023-02-06 18:54:25 +0300

    add new tests

--- tests/sample_requests/wrong_auth.sh
+++ tests/sample_requests/wrong_auth.sh
@@ -6,3 +6,9 @@ curl -X POST localhost:5000/vk \
 curl -X POST localhost:5000/vk \
     --data '{}' \
     -H "Content-type: application/json"
+curl -X POST localhost:5000/vk \
+    --data '{"type": "confirmation", "group_id": 217991594, "secret": "SmallSecret"}' \
+    -H "Content-type: application/json"
+curl -X POST localhost:5000/vk \
+    --data '{"type": "confirmation", "group_id": 21799, "secret": "BigSecret"}' \
+    -H "Content-type: application/json"
