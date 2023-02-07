4636f83ace8c1edb0620736240045028138af630
jojopko <gamerat.github@gmail.com>
2023-02-07 19:37:26 +0300

    anonimaze configs

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
+from dotenv import get_key
+
+DOTENV_FILE = ".env"
 
 class Configure(object):
-    SERVER_HOST = "localhost"
-    SERVER_PORT = "5000"
-    APP_SECRET = "BigSecret"
-    VK_CONFIRMATION = "0123456789abcdef"
-    VK_GROUP_ID = "217991594"
-    TG_CHANNEL_NAME = ""
+    SERVER_HOST = get_key(DOTENV_FILE, "SERVER_HOST")
+    SERVER_PORT = get_key(DOTENV_FILE, "SERVER_PORT")
+    APP_SECRET = get_key(DOTENV_FILE, "APP_SECRET")
+    VK_CONFIRMATION = get_key(DOTENV_FILE, "VK_CONFIRMATION")
+    VK_GROUP_ID = get_key(DOTENV_FILE, "VK_GROUP_ID")
+    TG_CHANNEL_NAME = get_key(DOTENV_FILE, "TG_CHANNEL_NAME")
+    TG_BOT_TOKEN = get_key(DOTENV_FILE, "TG_BOT_TOKEN")
