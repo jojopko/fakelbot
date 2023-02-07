249fe8340c4e101715a3035810926c3fcd5bd3c7
jojopko <gamerat.github@gmail.com>
2023-02-07 20:50:56 +0300

    update config.py

--- config.py
+++ config.py
@@ -1,12 +1,20 @@
 from dotenv import get_key
+import os
 
 DOTENV_FILE = ".env"
 
 class Configure(object):
-    SERVER_HOST = get_key(DOTENV_FILE, "SERVER_HOST")
-    SERVER_PORT = get_key(DOTENV_FILE, "SERVER_PORT")
-    APP_SECRET = get_key(DOTENV_FILE, "APP_SECRET")
-    VK_CONFIRMATION = get_key(DOTENV_FILE, "VK_CONFIRMATION")
-    VK_GROUP_ID = get_key(DOTENV_FILE, "VK_GROUP_ID")
-    TG_CHANNEL_NAME = get_key(DOTENV_FILE, "TG_CHANNEL_NAME")
-    TG_BOT_TOKEN = get_key(DOTENV_FILE, "TG_BOT_TOKEN")
+    SERVER_HOST = os.environ.get("SERVER_HOST") or \
+        get_key(DOTENV_FILE, "SERVER_HOST")
+    SERVER_PORT = os.environ.get("SERVER_PORT") or \
+        get_key(DOTENV_FILE, "SERVER_PORT")
+    APP_SECRET = os.environ.get("APP_SECRET") or \
+        get_key(DOTENV_FILE, "APP_SECRET")
+    VK_CONFIRMATION = os.environ.get("VK_CONFIRMATION") or \
+        get_key(DOTENV_FILE, "VK_CONFIRMATION")
+    VK_GROUP_ID = os.environ.get("VK_GROUP_ID") or \
+        get_key(DOTENV_FILE, "VK_GROUP_ID")
+    TG_CHANNEL_NAME = os.environ.get("TG_CHANNEL_NAME") or \
+        get_key(DOTENV_FILE, "TG_CHANNEL_NAME")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN") or \
+        get_key(DOTENV_FILE, "TG_BOT_TOKEN")
