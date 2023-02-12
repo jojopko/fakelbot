d47ab8c3904160a3e525431ca7174621cdf63798
jojopko <gamerat.github@gmail.com>
2023-02-12 13:02:44 +0300

    new types

--- fakel/vk/types.py
+++ fakel/vk/types.py
@@ -1,4 +1,21 @@
+from enum import Enum
 
 class UpdateTypes:
     CONFIRMATION = "confirmation"
     WALL_NEW_POST = "wall_post_new"
+    WALL_REPOST = "wall_repost"
+
+# https://dev.vk.com/reference/objects/photo-sizes
+# От меньшего до большего
+PHOTO_SIZES = {
+    "s" : 0,
+    "m" : 1,
+    "x" : 2,
+    "o" : -1,
+    "p" : -1,
+    "q" : -1,
+    "r" : -1,
+    "y" : 3,
+    "z" : 4,
+    "w" : 5
+}
