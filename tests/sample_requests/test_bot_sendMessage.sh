8915a4d0c0a48a290a6627c4647a14fa7bce6a0d
jojopko <gamerat.github@gmail.com>
2023-02-14 19:42:47 +0300

    add new tests

--- tests/sample_requests/test_bot_sendMessage.sh
+++ tests/sample_requests/test_bot_sendMessage.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-curl 'https://api.telegram.org/bot5902426033:AAGG7wqwHXno8chmJnM82qF7X095szfHP_A/sendMessage?chat_id=@test_channel_jjpk&text=test'
+# curl 'https://api.telegram.org/bot5902426033:AAGG7wqwHXno8chmJnM82qF7X095szfHP_A/sendMessage?chat_id=@test_channel_jjpk&text=test'
+curl 'https://api.telegram.org/bot5902426033:AAGG7wqwHXno8chmJnM82qF7X095szfHP_A/sendPhoto?chat_id=@test_channel_jjpk&photo=https://sun9-29.userapi.com/impg/eg6FwCvRvxjg_Xo7UyH0_fTF3q3xP2fhb3sShw/YUImx2e5vGw.jpg?size=1500x1500&quality=95&sign=2b144cbd0c0c206dc4d043df84a35d58&c_uniq_tag=OV-5UCHLFyXY5NfdbBzz_RxddjVQGhdilmEDWURgXA8&type=album'
